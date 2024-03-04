0e3e4bb7a21c59da752882dfe63d9a1bd04aafa7
Precious <preshengr@gmail.com>
2024-03-04 14:21:38 +0100

    Phone number Expression

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-# A regular expression that is matches a string that starts with h ends with n and can have any single character in between
-puts ARGV[0].scan(/h.n/).join
+# A regular expression that is matches 10 digit phone number
+puts ARGV[0].scan(/^[0-9]{10}$/).join
 
